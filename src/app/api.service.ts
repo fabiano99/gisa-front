2667ae2c0d8305fc4efe99f608bdbb9779643ac1
Fabiano Moreira <fabianomoreira@NT0887407MT1580.via.varejo.corp>
2024-06-14 17:17:34 -0300

    remove mock do chat

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -62,33 +62,33 @@ export class ApiService {
   }
 
   chatPrompt(prompt: string): Observable<{response: string, grafico?: BarChart[]}> {
-    // return this.http.post<any>(`${this.baseUrl}/chat`, {
-    //   "query": {
-    //     "user_query": prompt
-    //   },
-    //   "db_config": {
-    //     "host": "localhost",
-    //     "port": 3306,
-    //     "user": "user",
-    //     "password": "user",
-    //     "database": "Pedidos"
-    //   }
-    // });
+    return this.http.post<any>(`${this.baseUrl}/chat`, {
+      "query": {
+        "user_query": prompt
+      },
+      "db_config": {
+        "host": "localhost",
+        "port": 3306,
+        "user": "user",
+        "password": "user",
+        "database": "Pedidos"
+      }
+    });
 
     // mock return
-    return new Observable((observer) => {
-      setTimeout(() => {
-        observer.next({
-          response: '"Este erro ocorreu porque você inseriu uma questão no lugar de uma instrução SQL. A pergunta \"What are the 5 products with the highest total order value?\" não é uma sintaxe SQL válida, portanto, o interpretador SQL lançou um erro.\n\nAqui está a instrução SQL correta, sem a pergunta:\n\n```sql\nSELECT Nome_produto, SUM(Valor_total) as total_order_value \nFROM pedidos_novo \nGROUP BY Nome_produto \nORDER BY total_order_value DESC \nLIMIT 5;\n```\n\nTente executar apenas essa instrução SQL e você deverá obter os resultados desejados."',
-          grafico: [
-                { Eixo_x: 34, Eixo_y: 'Fevereiro' },
-                { Eixo_x: 31, Eixo_y: 'Março' },
-                { Eixo_x: 36, Eixo_y: 'Abril' },
-                { Eixo_x: 5, Eixo_y: 'Maio' }
-          ],
-        });
-      }, 1000);
-    });
+    // return new Observable((observer) => {
+    //   setTimeout(() => {
+    //     observer.next({
+    //       response: '"Este erro ocorreu porque você inseriu uma questão no lugar de uma instrução SQL. A pergunta \"What are the 5 products with the highest total order value?\" não é uma sintaxe SQL válida, portanto, o interpretador SQL lançou um erro.\n\nAqui está a instrução SQL correta, sem a pergunta:\n\n```sql\nSELECT Nome_produto, SUM(Valor_total) as total_order_value \nFROM pedidos_novo \nGROUP BY Nome_produto \nORDER BY total_order_value DESC \nLIMIT 5;\n```\n\nTente executar apenas essa instrução SQL e você deverá obter os resultados desejados."',
+    //       grafico: [
+    //             { Eixo_x: 34, Eixo_y: 'Fevereiro' },
+    //             { Eixo_x: 31, Eixo_y: 'Março' },
+    //             { Eixo_x: 36, Eixo_y: 'Abril' },
+    //             { Eixo_x: 5, Eixo_y: 'Maio' }
+    //       ],
+    //     });
+    //   }, 1000);
+    // });
   }
 
   getInsight(): Observable<{insight: string}> {
